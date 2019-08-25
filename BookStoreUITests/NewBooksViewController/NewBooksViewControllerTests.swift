9bb2cbce07468f747f576ad196d6f8471c7159e7
Soojin Ro <sugarpoint33@gmail.com>
2019-08-25 17:38:44 +0900

    NewBooksViewController & BookInfoViewController UI Tests

--- BookStoreUITests/NewBooksViewController/NewBooksViewControllerTests.swift
+++ BookStoreUITests/NewBooksViewController/NewBooksViewControllerTests.swift
@@ -10,13 +10,16 @@ import XCTest
 @testable import BookStore
 import Swifter
 
-private let itBookStoreHost = "api.itbook.store"
 private let newBooksPath = "/1.0/new"
+private let bookInfoPath = "/1.0/books/9781788476249"
+private let tableViewIdentifier = "NewBooksTableView"
+private let normalResponseJSONFilename = "NewBooksNormal.json"
+private let emptyResponseJSONFilename = "NewBooksEmpty.json"
 
 class NewBooksViewControllerTests: XCTestCase {
     var app = XCUIApplication()
     let server = HttpServer()
-
+    
     override func setUp() {
         continueAfterFailure = false
         
@@ -30,15 +33,81 @@ class NewBooksViewControllerTests: XCTestCase {
 
     func testNewBooksNormal() {
         do {
-            let path = try TestUtil.path(for: "NewBooksNormal.json", in: type(of: self))
-            server["/1.0/new"] = shareFile(path)
+            let path = try TestUtil.path(for: normalResponseJSONFilename, in: type(of: self))
+            server[newBooksPath] = shareFile(path)
             try server.start()
             app.launch()
         } catch {
             XCTAssert(false, "Swifter Server failed to start.")
         }
         
-        XCTAssert(app.staticTexts["9781788476249"].waitForExistence(timeout: 5))
-        XCTAssert(app.staticTexts["$44.99"].waitForExistence(timeout: 5))
+        assertTableViewResult()
+    }
+    
+    func testNewBooksEmpty() {
+        do {
+            let path = try TestUtil.path(for: emptyResponseJSONFilename, in: type(of: self))
+            server[newBooksPath] = shareFile(path)
+            try server.start()
+            app.launch()
+        } catch {
+            XCTAssert(false, "Swifter Server failed to start.")
+        }
+        
+        XCTAssert(app.staticTexts["Nothing new 🙃"].waitForExistence(timeout: 3))
+    }
+    
+    func testNewBooksErrorWithRetry() {
+        do {
+            server[newBooksPath] = { _ in
+                return HttpResponse.internalServerError
+            }
+            try server.start()
+            app.launch()
+        } catch {
+            XCTAssert(false, "Swifter Server failed to start.")
+        }
+        
+        XCTAssert(app.staticTexts["Something went wrong 🤯"].waitForExistence(timeout: 3))
+        XCTAssert(app.buttons["Try Again"].exists)
+        
+        do {
+            let path = try TestUtil.path(for: normalResponseJSONFilename, in: type(of: self))
+            server[newBooksPath] = shareFile(path)
+        } catch {
+            XCTAssert(false, "Couldn't find NewBooksNormal.json file.")
+        }
+        
+        app.buttons["Try Again"].tap()
+
+        assertTableViewResult()
+    }
+    
+    func testNewBookCellTap() {
+        do {
+            let path = try TestUtil.path(for: normalResponseJSONFilename, in: type(of: self))
+            server[newBooksPath] = shareFile(path)
+            server[bookInfoPath] = { _ in
+                return HttpResponse.internalServerError
+            }
+            try server.start()
+            app.launch()
+        } catch {
+            XCTAssert(false, "Swifter Server failed to start.")
+        }
+        
+        XCTAssert(app.tables[tableViewIdentifier].waitForExistence(timeout: 3))
+        app.tables[tableViewIdentifier].cells.firstMatch.tap()
+        
+        XCTAssert(app.buttons["Close"].waitForExistence(timeout: 3))
+    }
+    
+    private func assertTableViewResult() {
+        XCTContext.runActivity(named: "Test Successful TableView Screen") { _ in
+            XCTAssert(app.tables[tableViewIdentifier].waitForExistence(timeout: 3))
+            XCTAssert(app.tables[tableViewIdentifier].cells.count > 0)
+            XCTAssert(app.staticTexts["9781788476249"].exists)
+            XCTAssert(app.staticTexts["$44.99"].exists)
+        }
     }
 }
--- BookStoreUITests/Utils.swift
+++ BookStoreUITests/Utils.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import XCTest
 
 enum TestUtilError: Error {
     case fileNotFound
